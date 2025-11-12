293adcdf4caa3a248197a920122be59a4c286b8c
Purushotham Bollepalli <49681539-p3423689@users.noreply.replit.com>
2025-11-12 19:54:40 +0000

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
+    build:...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
